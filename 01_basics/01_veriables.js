fc8f9a9beba8c49d4caada6886bd22b128383e73
nirajganatra <161001638+nirajganatra@users.noreply.github.com>
2024-10-20 11:01:41 +0000

    dataTypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail =  "niraj19888@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2     not allowed 
+
+accountEmail = "fkd@yopmail.com"
+accountPassword = "5464664"
+accountCity = "rajkot"
+let accountState;
+
+/* 
+prefer not to use var 
+because of issue in block scope and function scope 
+ */
+
+console.log([accountId , accountEmail ,accountPassword , accountCity, accountState]);
+
---
+++ 01_basics/02_dataTypes
@@ -0,0 +1,20 @@
+"use strict"  // treat all js code as newer version 
+
+// alert( 3 + 3)    // we are using nodjs, not browser
+
+let name = "hites"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undifined =>
+// symbol 
+
+//object
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)  // object
