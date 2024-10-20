c616e7b5919d466e888b05bd4bcf3e0b4acda631
nirajganatra <161001638+nirajganatra@users.noreply.github.com>
2024-10-20 18:08:38 +0000

    strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,43 @@
+const name = "niarj"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("niraj")
+
+// console.log(gameName[0]);  n
+// console.log(gameName.__proto__);  {}
+
+
+// console.log(gameName.length); 5
+// console.log(gameName.toUpperCase()); NIRAJ
+// console.log(gameName.charAt(2))  r
+// console.log(gameName.indexOf('j')); 4
+
+// console.log(gameName.substring(0,4));  nira
+
+const anotherString = gameName.slice(-4,4) 
+
+// console.log(anotherString); ira
+
+const newStringOne = "   niraj    "
+
+// console.log(newStringOne);  "   niraj    "
+// console.log(newStringOne.trim()); niraj
+
+const url = "https://niraj.com/niraj%20ganatra";
+
+// console.log(url.replace('%20','-')); niraj-ganatra
+
+// console.log(url.includes('niraj'))  true
+// console.log(url.includes('ramesh'))   false
+
+
+const newName = "niraj-s-ganatra"
+// console.log(newName.split('-'));
+
+
+
+
+
