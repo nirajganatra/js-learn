5a66f209ebbbc72d6280e2de1f234aacd5366484
nirajganatra <161001638+nirajganatra@users.noreply.github.com>
2024-10-20 12:43:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+ let score = 33
+
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let score1 = "33"
+
+ let valueInNumber  = Number(score1);
+
+ console.log(typeof score);  // 33 
+
+ let score2 = "33adf"
+
+ let valueInNumber1 = Number(score2)
+
+ console.log(typeof valueInNumber1); 
+ console.log(valueInNumber1); // NaN
+
+ //true => 1; false => 0
+
+ let isLoggedIn = 1 
+
+ let BooleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(BooleanIsLoggedIn);  // true
+
+let isLoggedIn2 = "niraj"
+let BooleanIsLoggedIn2 = Boolean(isLoggedIn2);
+console.log(BooleanIsLoggedIn2); //true
+
+let isLoggedIn3 = ""
+let BooleanIsLoggedIn3 = Boolean(isLoggedIn3)
+console.log(BooleanIsLoggedIn3); //false
+
+// 1 => true; 0 => false
+//  "" => false
+// "niraj" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) // 33
+console.log(typeof stringNumber) //string
+
+
+ 
